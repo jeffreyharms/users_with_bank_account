aa52883886e41ce44b33e275ca5c68fa920d36db
Jeff Harms <jeffdharms@gmail.com>
2022-06-09 14:20:50 -0700

    tinkering with the program

--- user_bank_account.py
+++ user_bank_account.py
@@ -1,38 +1,72 @@
 class BankAccount:
+    
     list_accounts = []
-    # don't forget to add some default values for these parameters!
+    
     def __init__(self, int_rate, balance): 
         self.int_rate = int_rate
         self.balance = balance
         BankAccount.list_accounts.append(self)
-        # your code here! (remember, instance attributes go here)
-        # don't worry about user info here; we'll involve the User class soon
+        
     def deposit(self, amount):
         self.balance += amount
+        
     def withdraw(self, amount):
         self.balance -= amount
+        
     def display_account_info(self):
         print("Balance: ", self.balance)
+        
     def yield_interest(self):
         self.balance += self.balance*self.int_rate
+        
     @staticmethod
     def overdrawn(balance, amount):
         if ((balance - amount) < 0):
             return False
         else:
             return True
+        
     @classmethod
     def change_bank_name(cls, name):
         cls.bank_name = name
+        
     @classmethod
     def all_balances(cls):
         sum = 0
-        for account in cls.all_accounts:
-            sum += account.balance
+        for object in cls.list_accounts:
+            sum += object.balance
         return sum 
+    
+class User:
+    list_users = []
+    bank_name = "Bank of the Weast"
+    def __init__(self, name, email_address):
+        self.name = name
+        self.email_address = email_address
+        self.account = BankAccount(0.03, 150)
+        User.list_users.append(self)
+        
+    def display_user_balance(self):
+        print("Balance: ", self.account.balance)
+        
+    @classmethod
+    def all_users(cls):
+        sum = 0
+        for object in cls.list_users:
+            print(object)
+            
+Hilde = User("Hilde", "hild@antisocial.com")
+Gaston = User("Gaston", "noonefightslikegaston@neckbelts.com")
 
-account_1 = BankAccount(0.03, 5000)
-account_2 = BankAccount(0.05, 20)
+Gaston.account.change_bank_name("E Corp FCU")
 
-account_1.deposit(250).deposit(500).deposit(750).withdraw(1000).display_account_info()
-account_2.deposit(400).deposit(350).withdraw(25).withdraw(290).withdraw(50).withdraw(200).yield_interest().display_account_info()
+Gaston.account.deposit(500)
+
+Gaston.display_user_balance()
+
+
+# account_1 = BankAccount(0.03, 5000)
+# account_2 = BankAccount(0.05, 20)
+
+# account_1.deposit(250).deposit(500).deposit(750).withdraw(1000).display_account_info()
+# account_2.deposit(400).deposit(350).withdraw(25).withdraw(290).withdraw(50).withdraw(200).yield_interest().display_account_info()
